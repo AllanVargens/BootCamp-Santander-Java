2459fe078bb2228edb3a5a7896d51de4a0987f95
AllanVargens <allanvargens@gmail.com>
2024-06-18 02:00:03 -0300

    feat: doc com swagger

--- SpringBoot/my-first-web-api/src/main/java/dio/web/api/MyFirstWebApiApplication.java
+++ SpringBoot/my-first-web-api/src/main/java/dio/web/api/MyFirstWebApiApplication.java
@@ -3,7 +3,11 @@ package dio.web.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Web Api SpringBoot", version = "v1", description = "Api Spring Boot usada para estudos"))
 public class MyFirstWebApiApplication {
 
 	public static void main(String[] args) {
--- SpringBoot/my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
+++ SpringBoot/my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
@@ -13,18 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dio.web.api.model.Usuario;
 import dio.web.api.repository.UsuarioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User", description = "API para gestão de usuários")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
 
     @GetMapping()
+    @Operation(summary = "Lista todos os usuários", description = "Retorna uma lista de todos os usuários cadastrados", responses = {
+            @ApiResponse(responseCode = "200", description = "Sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Usuario.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public List<Usuario> getUsers() {
         return repository.findAll();
     }
 
+    @Operation(summary = "Lista usuario por nome", description = "Lista um usuario do sistema", responses = {
+            @ApiResponse(responseCode = "200", description = "Sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Usuario.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/{username}")
     public Usuario getOne(@PathVariable("username") String username) {
         return repository.findByUsername(username);
@@ -35,6 +49,10 @@ public class UsuarioController {
         repository.deleteById(id);
     }
 
+    @Operation(summary = "Cria um novo usuário", description = "Adiciona um novo usuário ao sistema", responses = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida")
+    })
     @PostMapping()
     public void postUser(@RequestBody Usuario usuario) {
         repository.save(usuario);
