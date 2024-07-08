0f227117d066f76f73252dacca6f9c77d885d8a5
AllanVargens <allanvargens@gmail.com>
2024-07-08 17:34:40 -0300

    feat: models and repository

---
+++ SpringBoot/santander-dev-week-2023/santander-dev-week-2023/src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,45 @@
+package me.dio.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_card")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String number;
+    @Column(name = "avaiable_limit", nullable = false, precision = 2, scale = 13)
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
+}
